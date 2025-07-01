5bcfaa9f8ccd249e02d5a644a4192bd4f0fc9746
Sunny Sharma <sunnysharma1397@gmail.com>
2025-07-01 08:54:11 +0530

    Update contact phone number in Footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -76,7 +76,7 @@ export default function Footer() {
                 <svg className="w-4 h-4 text-gray-300" fill="currentColor" viewBox="0 0 20 20">
                   <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                 </svg>
-                <span>+971 4 123 4567</span>
+                <span>+971 503 755 011</span>
               </li>
               <li className="flex items-center space-x-2 text-gray-200">
                 <svg className="w-6 h-6 text-gray-300" fill="currentColor" viewBox="0 0 24 24">
